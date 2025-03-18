204e3254ef84603490771bf69264e18aaa2c5cd0
Mr-DooSun <mrdoosun@gmail.com>
2025-03-18 22:12:28 +0900

    [#1] refac : response format 기능을 base_usecase에서 controller로 옮기기

--- src/core/applications/use_cases/base_use_case.py
+++ src/core/applications/use_cases/base_use_case.py
@@ -2,7 +2,6 @@
 from abc import ABC, abstractmethod
 from typing import List, Type, TypeVar
 
-from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
 from src.core.applications.responses.base_response import BaseResponse
@@ -10,7 +9,7 @@ from src.core.domain.services.base_service import BaseService
 
 CreateDTO = TypeVar("CreateDTO", bound=BaseModel)
 UpdateDTO = TypeVar("UpdateDTO", bound=BaseModel)
-ResponseTemplate = TypeVar("ResponseTemplate", bound=BaseResponse)
+ResponseDto = TypeVar("ResponseDto", bound=BaseResponse)
 
 
 class BaseUseCase(ABC):
@@ -27,7 +26,7 @@ class BaseUseCase(ABC):
 
     @property
     @abstractmethod
-    def response_template(self) -> Type[ResponseTemplate]:
+    def response_dto(self) -> Type[ResponseDto]:
         pass
 
     @property
@@ -35,37 +34,24 @@ class BaseUseCase(ABC):
     def update_dto(self) -> Type[UpdateDTO]:
         pass
 
-    async def create_data(self, create_data: CreateDTO) -> ResponseTemplate:
-        data = await self.base_service.create_data(create_data=create_data)
-        encoded_data = jsonable_encoder(data)
-        return self.response_template(data=encoded_data)
+    async def create_data(self, create_data: CreateDTO) -> ResponseDto:
+        return await self.base_service.create_data(create_data=create_data)
 
-    async def create_datas(
-        self, create_datas: List[CreateDTO]
-    ) -> List[ResponseTemplate]:
-        datas = await self.base_service.create_datas(create_datas=create_datas)
-        encoded_data = jsonable_encoder(datas)
-        return self.response_template(data=encoded_data)
+    async def create_datas(self, create_datas: List[CreateDTO]) -> List[ResponseDto]:
+        return await self.base_service.create_datas(create_datas=create_datas)
 
-    async def get_datas(self, page: int, page_size: int) -> List[ResponseTemplate]:
-        datas = await self.base_service.get_datas(page=page, page_size=page_size)
-        encoded_data = jsonable_encoder(datas)
-        return self.response_template(data=encoded_data)
+    async def get_datas(self, page: int, page_size: int) -> List[ResponseDto]:
+        return await self.base_service.get_datas(page=page, page_size=page_size)
 
-    async def get_data_by_data_id(self, data_id: int) -> ResponseTemplate:
-        data = await self.base_service.get_data_by_data_id(data_id=data_id)
-        encoded_data = jsonable_encoder(data)
-        return self.response_template(data=encoded_data)
+    async def get_data_by_data_id(self, data_id: int) -> ResponseDto:
+        return await self.base_service.get_data_by_data_id(data_id=data_id)
 
     async def update_data_by_data_id(
         self, data_id: int, update_data: UpdateDTO
-    ) -> ResponseTemplate:
-        data = await self.base_service.update_data_by_data_id(
+    ) -> ResponseDto:
+        return await self.base_service.update_data_by_data_id(
             data_id=data_id, update_data=update_data
         )
-        encoded_data = jsonable_encoder(data)
-        return self.response_template(data=encoded_data)
 
-    async def delete_data_by_data_id(self, data_id: int) -> ResponseTemplate:
+    async def delete_data_by_data_id(self, data_id: int):
         await self.base_service.delete_data_by_data_id(data_id=data_id)
-        return self.response_template()
