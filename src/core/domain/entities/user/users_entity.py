9a2a17be6898a5639f58e93912a3202bda5aa4f4
Mr-DooSun <mrdoosun@gmail.com>
2025-08-01 23:02:47 +0900

    chore : 오타 수정

--- src/core/domain/entities/user/users_entity.py
+++ src/core/domain/entities/user/users_entity.py
@@ -12,7 +12,7 @@ class CoreUsersEntity(Entity):
     full_name: str = Field(..., description="유저 이름")
     email: str = Field(..., description="유저 이메일")
     password: str = Field(..., description="유저 비밀번호")
-    create_at: datetime = Field(..., description="유저 생성 시간")
+    created_at: datetime = Field(..., description="유저 생성 시간")
     updated_at: datetime = Field(..., description="유저 수정 시간")
 
 
